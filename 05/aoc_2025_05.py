e870c33c2f2ea8a8fc156d708e93acbd6b790cc1
S-tefan <github@k-son.se>
2025-12-05 17:47:13 +0100

    day 5 two stars

--- 05/aoc_2025_05.py
+++ 05/aoc_2025_05.py
@@ -24,25 +24,28 @@ def fixdata(data):
             range_mode = False
     return ranges, ids
 
-def check(id, ranges):
-    for r in ranges:
-        if id in r:
-            return True
-    return False
+def normalize(ranges):
+    ranges.sort(key = lambda x: x.start)
+    k=0
+    while True:
+        if k == len(ranges)-1:
+            return
+        if ranges[k+1].start in ranges[k]:
+            ranges[k] = range(ranges[k].start,max(ranges[k].stop,ranges[k+1].stop))
+            ranges.pop(k+1)
+        else:
+            k += 1
         
-
-
 def partone(data):
     ranges, ids = fixdata(data)
     return len(list(filter(lambda x: check(x, ranges), ids)))
 
-
 def parttwo(data):
-    ranges, ids = fixdata(data)
-    # merge ranges ...
-
-    return None
+    ranges, _ = fixdata(data)
+    normalize(ranges)
+    return sum(len(r) for r in ranges)
 
 ap = AOC(5,'input')
 ap.runprint(partone)
 ap.runprint(parttwo)
+
