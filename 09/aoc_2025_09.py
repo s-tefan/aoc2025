b974590bc1c9bd17d7ee8463cf854e4885a908a5
S-tefan <github@k-son.se>
2025-12-14 17:01:36 +0100

    Experiment med dag 10 del2, inget som löst input på rimlig tid. Dag 9 inte heller klar.

--- 09/aoc_2025_09.py
+++ 09/aoc_2025_09.py
@@ -23,9 +23,42 @@ def partone(data):
             largest = max(largest, int((abs(p[0]-q[0])+1)*(abs(p[1]-q[1])+1)))
     return largest
 
+# part two does not work yet
 def parttwo(data):
-    return None
+    coords = fixdata(data)
+    areas = []
+    h_edges, v_edges = [], []
+    for k, p in enumerate(coords):
+        q = coords[(k-1)%len(coords)]
+        if p[0] == q[0]:
+            h_edges.append((p[0],min(p[1],q[1]),max(p[1],q[1])))
+        else:
+            v_edges.append((p[1],min(p[0],q[0]),max(p[0],q[0])))
 
-ap = AOC(9,'input')
+    for k, p in enumerate(coords):
+        for m, q in enumerate(coords[:k-1]):
+            c_min, c_max = (min(p[0],q[0]),min(p[1],q[1])), (max(p[0],q[0]),max(p[1],q[1]))
+
+            if not any( \
+                (v[0] in range(c_min[0]+1,c_max[0])) and (v[1] < c_max[1] or v[2] >= c_min[1]) \
+                for v in v_edges) or \
+                not any( \
+                (h[1] in range(c_min[1]+1,c_max[1]) and (h[1] <= c_max[0] or h[2] >= c_min[0])) \
+                for h in h_edges):
+                areas.append(int((abs(p[0]-q[0])+1)*(abs(p[1]-q[1])+1)))
+                print(p,q)
+
+            '''
+
+
+            if not any(c[0] in range(min(p[0],q[0])+1,max(p[0],q[0])) and \
+                        c[1] in range(min(p[1],q[1])+1,max(p[1],q[1])) \
+                            for c in coords):
+                areas.append(int((abs(p[0]-q[0])+1)*(abs(p[1]-q[1])+1)))
+                print(p,q)
+            '''
+    return max(areas)
+
+ap = AOC(9,'test')
 ap.runprint(partone)
 ap.runprint(parttwo)
--- 10/aoc_2025_10.py
+++ 10/aoc_2025_10.py
@@ -10,7 +10,7 @@ class AOC:
     def runprint(self, fun):
         print(fun(self.inp))
 
-def fixdata(data):
+def fixdata(data, part=1):
     val = []
     for line in data:
         bobs = line.split()
@@ -26,13 +26,17 @@ def fixdata(data):
             p = 0
             if isinstance(press, int):
                 press = (press,)
-            for b in press:
-                p ^= 1 << b
+            if part == 1:
+                for b in press:
+                    p ^= 1 << b
+            else:
+                p = press
             presses.append(p)
-        joltage = bobs[-1]
+        joltage = eval('['+bobs[-1][1:-1]+']')
         val.append((len(lightsyms), lightbits, presses, joltage))
     return val
 
+# Not used in latest version, used in first
 def combinations(m,k):
     # return a list of subsets of size k of set m
     if k == 0:
@@ -41,10 +45,23 @@ def combinations(m,k):
     n = len(m)
     return  [a|{e} for e in m for a in combinations(m-{e},k-1)]
 
+def intcombs(n,k):
+    s = []
+    for i in range(1,k+1):
+        if i == 1:
+            s = list([i] for i in range(n))
+        else:
+            s = [ a + [b] for a in s for b in range(a[-1]+1,n)]
+    return s
+
+
 
 def presses_needed(nl, light, presses):
     for np in range(1,nl+1):
-        pcombs = combinations(set(presses), np)
+        #pcombs = combinations(set(presses), np)
+        #print(pcombs)
+        pcombs = [[presses[i] for i in c] for c in intcombs(len(presses),np)]
+        #print(pcombs)
         for ps in pcombs:
             s = light
             for p in ps:
@@ -52,6 +69,68 @@ def presses_needed(nl, light, presses):
             if s == 0:
                 return np
     raise Exception("Data not compliant")
+
+'''
+def presses_needed2(presses, joltage): # fixa...
+    print('doing', joltage)
+    k = len(presses)
+    n = 1
+    while True:
+        print(n)
+        for c in intcombs(n+k-1,k-1):
+            jolt=[0]*len(joltage)
+            d = [-1] + c + [n+k-1]
+            ap = [d[i+1]-d[i]-1 for i in range(k)]
+            for j, a in enumerate(presses):
+                for b in a:
+                    jolt[b] += ap[j]
+            if jolt == joltage:
+                return n
+        n += 1
+'''
+
+def jolt(ap, b_counters, nc):
+    jolts = [0]*nc
+    for j in range(len(b_counters)):
+        for c in b_counters[j]:
+            jolts[c] += ap[j]
+    return jolts
+
+'''
+def presses_needed2(presses, joltage): # fixa...
+    print('doing', joltage)
+    k = len(presses)
+    n_pr = [[0]*k]
+    nj = len(joltage)
+    n = 0
+    while n_pr:
+        #print(len(n_pr))
+        n += 1
+        # update
+        ny_pr =[]
+        for ap in n_pr:
+            for i in range(k):
+                apa = ap.copy()
+                apa[i] += 1
+                j = jolt(apa, presses, nj)
+                #print(apa, j)
+                if j == joltage:
+                    return n
+                elif all(j[i] <= joltage[i] for i in range(nj)):
+                    ny_pr.append(apa)
+        n_pr = ny_pr
+    raise Exception("No hit")
+'''
+
+def presses_needed2(presses, joltage):
+    # tänkt att jobba mod p...
+    primes = [2,3,5,7,11,13,17,19,23,29]
+    m = [[0]*len(presses) for _ in range(len(joltage))]
+    print(presses)
+    for k, pr in enumerate(presses):
+        for j in pr:
+            m[j][k] = 1
+    print(m)
 
 
 
@@ -61,9 +140,9 @@ def partone(data):
 
 
 def parttwo(data):
-    return None
+    for _, _, presses, joltage in fixdata(data, part = 2):
+        a = presses_needed2(presses, joltage) 
 
-
-ap = AOC(10,'input')
+ap = AOC(10,'test')
 ap.runprint(partone)
 ap.runprint(parttwo)
